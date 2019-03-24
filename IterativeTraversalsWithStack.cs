524079584b1a5985811274c5839c3f187c55d0aa
Preeti Lolla <preetilolla@users.noreply.github.com>
2019-03-24 13:31:30 +0530

    Update and rename IterativeInorder.cs to IterativeTraversalsWithStack.cs

--- IterativeTraversalsWithStack.cs
+++ IterativeTraversalsWithStack.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PS
 {
+	// Iterative traversals with Stack.
 	class IterativeInorder
 	{
 		static void Main(string[] args)
@@ -12,6 +13,8 @@ namespace PS
 			root.Left = new Node(2, new Node(1), new Node(3));
 			root.Right = new Node(7, new Node(6), new Node(8));
 			getIterativeInorder(root);
+			getIterativePreOrder(root);
+			getIterativePostOrder(root);
 			Console.ReadKey();
 		}
 
@@ -38,6 +41,68 @@ namespace PS
 
 			Console.WriteLine("The inorder traversal is..");
 			foreach (int i in inorder)
+			{
+				Console.Write(i + " ");
+			}
+		}
+
+		static void getIterativePreOrder(Node root)
+		{
+			List<int> preorder = new List<int>();
+			Stack<Node> s = new Stack<Node>();
+			Node current = root;
+
+			while (!(s.Count == 0))
+			{
+				current = s.Pop();
+				if (current != null)
+				{
+					preorder.Add(current.Data);
+					s.Push(current.Right);
+					s.Push(current.Left);
+				}
+			}
+
+			Console.WriteLine("\n\nThe preorder traversal is..");
+			foreach (int i in preorder)
+			{
+				Console.Write(i + " ");
+			}
+		}
+
+		static void getIterativePostOrder(Node root)
+		{
+			List<int> postorder = new List<int>();
+			Stack<Node> s = new Stack<Node>();
+			Node current = root;
+
+			while (true)
+			{
+				while (current != null)
+				{
+					s.Push(current);
+					s.Push(current);
+					current = current.Left;
+				}
+				if (s.Count == 0)
+				{
+					break;
+				}
+				current = s.Pop();
+				
+				if (s.Count != 0 && s.Peek() == current)
+				{
+					current = current.Right;
+				}
+				else
+				{
+					postorder.Add(current.Data);
+					current = null;
+				}
+			}
+
+			Console.WriteLine("\n\nThe postorder traversal is..");
+			foreach (int i in postorder)
 			{
 				Console.Write(i + " ");
 			}
