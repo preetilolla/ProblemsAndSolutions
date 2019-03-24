44f8b53361e7974c34f8e58e8ceb1c1413622c99
Preeti Lolla <preetilolla@users.noreply.github.com>
2019-03-24 15:20:42 +0530

    Update IterativeTraversalWithoutStack.cs

--- IterativeTraversalWithoutStack.cs
+++ IterativeTraversalWithoutStack.cs
@@ -71,64 +71,12 @@ namespace PSIT
 
 		static void getIterativePreOrder(Node root)
 		{
-			List<int> preorder = new List<int>();
-			Stack<Node> s = new Stack<Node>();
-			Node current = root;
-
-			while (!(s.Count == 0))
-			{
-				current = s.Pop();
-				if (current != null)
-				{
-					preorder.Add(current.Data);
-					s.Push(current.Right);
-					s.Push(current.Left);
-				}
-			}
-
-			Console.WriteLine("\n\nThe preorder traversal is..");
-			foreach (int i in preorder)
-			{
-				Console.Write(i + " ");
-			}
+			
 		}
 
 		static void getIterativePostOrder(Node root)
 		{
-			List<int> postorder = new List<int>();
-			Stack<Node> s = new Stack<Node>();
-			Node current = root;
-
-			while (true)
-			{
-				while (current != null)
-				{
-					s.Push(current);
-					s.Push(current);
-					current = current.Left;
-				}
-				if (s.Count == 0)
-				{
-					break;
-				}
-				current = s.Pop();
-
-				if (s.Count != 0 && s.Peek() == current)
-				{
-					current = current.Right;
-				}
-				else
-				{
-					postorder.Add(current.Data);
-					current = null;
-				}
-			}
-
-			Console.WriteLine("\n\nThe postorder traversal is..");
-			foreach (int i in postorder)
-			{
-				Console.Write(i + " ");
-			}
+			
 		}
 	}
 
